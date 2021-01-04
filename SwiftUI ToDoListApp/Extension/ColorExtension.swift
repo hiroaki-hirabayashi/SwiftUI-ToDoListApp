05de8ceac0bda8d2623702822abbd191c8b8d21f
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-01-04 17:32:13 +0900

    ユーザー画面のUI作成

--- SwiftUI ToDoListApp/Extension/ColorExtension.swift
+++ SwiftUI ToDoListApp/Extension/ColorExtension.swift
@@ -9,10 +9,10 @@
 import SwiftUI
 
 extension Color {
-    static let tBackground = Color(#colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1))
-    static let tRed = Color(#colorLiteral(red: 1, green: 0.2902, blue: 0.4745, alpha: 1))
-    static let tBlue = Color(#colorLiteral(red: 0.0706, green: 0.6392, blue: 0.9922, alpha: 1))
-    static let tYellow = Color(#colorLiteral(red: 1, green: 0.7569, blue: 0.2235, alpha: 1))
-    static let tGreen = Color(#colorLiteral(red: 0.2157, green: 0.7255, blue: 0.5804, alpha: 1))
-    static let tTitle = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+    static let selfBackground = Color(#colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1))
+    static let selfRed = Color(#colorLiteral(red: 1, green: 0.2902, blue: 0.4745, alpha: 1))
+    static let selfBlue = Color(#colorLiteral(red: 0.0706, green: 0.6392, blue: 0.9922, alpha: 1))
+    static let selfYellow = Color(#colorLiteral(red: 1, green: 0.7569, blue: 0.2235, alpha: 1))
+    static let selfGreen = Color(#colorLiteral(red: 0.2157, green: 0.7255, blue: 0.5804, alpha: 1))
+    static let selfTitle = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
 }
---
+++ SwiftUI ToDoListApp/UserView.swift
@@ -0,0 +1,45 @@
+//
+//  UserView.swift
+//  SwiftUI ToDoListApp
+//
+//  Created by 平林 宏淳 on 2021/01/04.
+//  Copyright © 2021 平林 宏淳. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    
+    //MARK: - Properties
+    let image: Image
+    let userName: String
+    
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading) { //左寄せ
+                Text("こんにちわ")
+                    .foregroundColor(Color.selfTitle) //文字色に自作した色指定
+                    .font(.footnote)
+                Text("\(userName)")
+                    .foregroundColor(Color.selfTitle) //文字色に自作した色指定
+                    .font(.title)
+            }
+            Spacer()
+            image
+                .resizable() //サイズ変更可能に
+                .frame(width: 60, height: 60)
+                .clipShape(Circle())
+        }
+        .padding() //HStackに余白を入れる
+        .background(Color.selfBackground) //背景色に自作した色指定
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            UserView(image: Image("Catalina"), userName: "User Name")
+            Circle()
+        }
+    }
+}
