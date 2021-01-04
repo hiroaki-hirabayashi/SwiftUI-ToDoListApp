54f4a2d8cbdb9656c2b40f2d40c9b601d89dc03e
Hiroaki Hirabayashi <h-hirabayashi@rough-and-ready.co.jp>
2021-01-04 13:23:43 +0900

    Initial Commit

---
+++ SwiftUI ToDoListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI ToDoListApp
+//
+//  Created by 平林 宏淳 on 2021/01/04.
+//  Copyright © 2021 平林 宏淳. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
