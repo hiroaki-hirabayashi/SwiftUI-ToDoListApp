29b37eb3a9fd3a4c426f4997a34f60763d9190d8
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-01-05 11:28:03 +0900

    QuickNewTask.swift追加

---
+++ SwiftUI ToDoListApp/QuickNewTask.swift
@@ -0,0 +1,21 @@
+//
+//  QuickNewTask.swift
+//  SwiftUI ToDoListApp
+//
+//  Created by 平林 宏淳 on 2021/01/05.
+//  Copyright © 2021 平林 宏淳. All rights reserved.
+//
+
+import SwiftUI
+
+struct QuickNewTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuickNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        QuickNewTask()
+    }
+}
