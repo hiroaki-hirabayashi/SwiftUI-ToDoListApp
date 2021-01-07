8812019f247ff4e7eee7e6c6794c71c4b40b1b15
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-01-08 01:55:37 +0900

    TodayToDo.swift追加（メイン画面に表示させる一覧リスト）

---
+++ SwiftUI ToDoListApp/Extension/DateExtention.swift
@@ -0,0 +1,25 @@
+//
+//  DateExtention.swift
+//  SwiftUI ToDoListApp
+//
+//  Created by 平林 宏淳 on 2021/01/08.
+//  Copyright © 2021 平林 宏淳. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    /// 「時分秒」を切り落とした今日の日付を作成
+    static var todayDate : Date {
+        let calendar = Calendar(identifier: .gregorian)
+        let time = Date()
+        let today = calendar.startOfDay(for: time)
+        return today
+    }
+    /// 「時分秒」を切り落とした明日の日付を作成
+    static var tomorrowDate : Date {
+        let calendar = Calendar(identifier: .gregorian)
+        let tomorrow = calendar.date(byAdding: DateComponents(day: 1), to: Date.todayDate)!
+        return tomorrow
+    }
+}
---
+++ SwiftUI ToDoListApp/TodayToDo.swift
@@ -0,0 +1,40 @@
+//
+//  TodayToDo.swift
+//  SwiftUI ToDoListApp
+//
+//  Created by 平林 宏淳 on 2021/01/08.
+//  Copyright © 2021 平林 宏淳. All rights reserved.
+//
+
+import SwiftUI
+
+// メイン画面に表示させる一覧リスト
+struct TodayToDo: View {
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \ToDoEntity.time,
+                                           ascending: true)],
+        predicate: NSPredicate(format:"time BETWEEN {%@ , %@}", Date.todayDate as NSDate, Date.tomorrowDate as NSDate),
+        animation: .default)
+    
+    var toDoList: FetchedResults<ToDoEntity>
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("本日のToDo")
+                .font(.footnote).bold().padding()
+            List(toDoList) { toDo in
+                ToDoDetailRow(observedToDo: toDo)
+            }
+        }.background(Color(UIColor.systemBackground))
+        
+    }
+}
+
+struct TodayToDo_Previews: PreviewProvider {
+    static var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    static var previews: some View {
+        TodayToDo()
+            .environment(\.managedObjectContext, context)
+    }
+}
