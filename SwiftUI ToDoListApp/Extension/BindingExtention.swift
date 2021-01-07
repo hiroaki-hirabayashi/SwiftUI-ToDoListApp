3d06eca9bcd6ad72fe0673926a2a0399d66ae0ca
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-01-07 11:41:20 +0900

    ToggleのON OFFで入力未入力を切り替えたいのでBindingExtention追加

---
+++ SwiftUI ToDoListApp/Extension/BindingExtention.swift
@@ -0,0 +1,21 @@
+//
+//  BindingExtention.swift
+//  SwiftUI ToDoListApp
+//
+//  Created by 平林 宏淳 on 2021/01/07.
+//  Copyright © 2021 平林 宏淳. All rights reserved.
+//
+
+import SwiftUI
+
+struct BindingExtention: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingExtention_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingExtention()
+    }
+}
