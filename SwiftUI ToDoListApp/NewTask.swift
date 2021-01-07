e6bbd0ba731191459b44c1e93e5c6afe949d7334
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-01-07 11:23:44 +0900

    NewTask.swiftにForm追加

--- SwiftUI ToDoListApp/NewTask.swift
+++ SwiftUI ToDoListApp/NewTask.swift
@@ -12,14 +12,20 @@ struct NewTask: View {
 
     //MARK: - Properties
     @State var newTask = ""
-    @State var dateTime = Date()
+    @State var selectDateTime = Date()
     
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 TextField("ToDo(やる事)追加", text: $newTask)
                 // selectionには渡すBinding
-                DatePicker(selection: $dateTime, label: { Text("日時") })
+                DatePicker(selection: $selectDateTime, label: { Text("日時") })
+                Button(action: {}) {
+                    HStack(alignment: .center) {
+                        Image(systemName: "minus.circle.fill")
+                        Text("キャンセル")
+                    }.foregroundColor(.red)
+                }
             }.navigationBarTitle("ToDo追加")
         }
     }
@@ -28,5 +34,7 @@ struct NewTask: View {
 struct NewTask_Previews: PreviewProvider {
     static var previews: some View {
         NewTask()
+            .environment(\.locale, Locale(identifier: "ja_JP")) // NewTask_Previews上で日本語化
+
     }
 }
